10c319d2dc9f38221f6b6f0f277269e7c9a18062
Julie <jjack109@odu.edu>
2023-03-18 04:22:58 -0400

    comments

--- project/src/main/java/edu/odu/cs/cs350/pne/SemesterReport.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/SemesterReport.java
@@ -2,8 +2,15 @@ package edu.odu.cs.cs350.pne;
 
 public class SemesterReport {
 
-    char marker;
+    char marker;        // represents the projected enrollment 
 
+    /**
+     * If the projected enrollment is greater than the cap, then an 
+     * asterisk is returned. Otherwise, a blank space is returned.
+     * @param projected the number of students projected to enroll 
+     * @param cap the maximum number of students able to enroll
+     * @return char for projected enrollment
+     */
     public char projectedEnrollment(int projected, int cap) {
 
         if(projected > cap) {
@@ -15,11 +22,17 @@ public class SemesterReport {
 
     }
 
+    /**
+     * Prints the headers for the summary projection report
+     */
     public String toString() {
 
         String header = String.format("%-1s %-10s %-15s %-15s %-15s", 
                         " ", "Course", "Enrollment", "Projected", "Cap");
         
+        // "observable" behavior
+        System.out.println(header);
+
         return header;
     }
 }
