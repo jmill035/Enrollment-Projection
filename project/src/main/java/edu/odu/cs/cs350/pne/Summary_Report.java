975d6ceff59cc8e8772117fd4ea0a8bce35a2a58
Kofi Amoah <kamoa001@odu.edu>
2023-03-18 00:13:03 -0400

    kofi-sumJavacode

--- project/src/main/java/edu/odu/cs/cs350/pne/Summary_Report.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Summary_Report.java
@@ -1,6 +1,36 @@
+package edu.odu.cs.cs350.pne;
+
+import java.util.Scanner;
+
+public class TestEnrollementPercentage {
+
+    public static double getEnrollmentPercentage (Enrollement begin, Enrollement end ) {
+
+    Enrollement start = Enrollement.now();
+
+    //Registration end_Date = EndDate.now();
+
+    int EnrollmentPercentage = end.getRegistrationdate() - start.getRegistrationDate();
+    //     int EnrollmentPercentage = end.Enrollement/...date() - start.Enrollement/..date();
+
+
+    double percentage =((double) EnrollmentPercentage) * 100;
+
+    return percentage;
+}
+
+ 
+
+ System.out.println("Percentage of enrollment period that has passed:" + percentage + "%");
+
+ 
+
+}
 
 
 
-public class Summary_Report
+
+
+
 
 // function "to supply past & current semester directories in the CLI"
