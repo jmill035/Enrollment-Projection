1558231ad355053a7147b2a79858a4c85632e3c3
eturkogl <eturk004@odu.edu>
2023-03-18 18:53:18 -0400

    Adding files

---
+++ project/src/test/java/edu/odu/cs/cs350/pne/SemesterTest.java
@@ -0,0 +1,18 @@
+package edu.odu.cs.cs350.pne;
+
+import org.junit.jupiter.api.Test;
+
+public class SemesterTest {
+
+ @Test
+ public void testSupplyDirectories(){
+
+    Semester semester = new Semester();
+    String pastDirectory = "path/to/past/directory" ;
+    String currentDirectory = "path/to/current/directory" ; 
+
+    assertTrue(semester.supplyDirectories(pastDirectory, currentDirectory));
+
+ }
+ 
+}
