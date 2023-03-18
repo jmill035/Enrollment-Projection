cb2f4456eb153214f2eda19216b27a316cce8e11
r3d3k3r <lb3attd@gmail.com>
2023-03-18 18:38:59 -0400

    compilation error fix 1

--- project/src/main/java/edu/odu/cs/cs350/pne/IOReader.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/IOReader.java
@@ -4,10 +4,10 @@ public class IOReader {
     public static void ReadExcelSheets(String[] args) {
         String filePath = "src/data/test.xlsx";
         try {
-            FileInputStream inputStream = new FileInputStream(new File(filePath))
+            FileInputStream inputStream = new FileInputStream(new File(filePath));
             Workbook workbook = WorkbookFactory.create(inputStream);
             int numberOfSheets = workbook.getNumberOfSheets();
-            for (int i = 0; i < numberOfSheets; i++;); {
+            for (int i = 0; i < numberOfSheets; i++) {
                 Sheet sheet = workbook.getSheetAt(i);
                 System.out.println("Sheet Name: " + sheet.getSheetName());
                 for (Row row : sheet) { 
