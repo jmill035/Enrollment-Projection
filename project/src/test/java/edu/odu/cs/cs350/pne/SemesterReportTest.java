9dd6c5b77e42be0607dbc4f5d9dd0578f8fe27f4
Julie <jjack109@odu.edu>
2023-03-18 00:15:11 -0400

    test1

---
+++ project/src/test/java/edu/odu/cs/cs350/pne/SemesterReportTest.java
@@ -0,0 +1,22 @@
+package edu.odu.cs.cs350.pne;
+
+import org.junit.jupiter.api.Test;
+
+public class SemesterReportTest {
+    
+    @Test
+    public void hearderOneTest() {
+        char asterick = '*';
+        char space = ' ';
+        assertThat(projectedEnrollment(88, 75), is('*'));
+
+
+    }
+
+
+
+    @Test
+    public void toStringTest() {
+
+    }
+}
