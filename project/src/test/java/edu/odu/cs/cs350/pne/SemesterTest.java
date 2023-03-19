ac866aa90bf73406c55b12bf3729db77635c684e
Ryan <rtran004@odu.edu>
2023-03-18 23:04:30 -0400

    wrote test code for supplyCutoff

--- project/src/test/java/edu/odu/cs/cs350/pne/SemesterTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/SemesterTest.java
@@ -1,6 +1,8 @@
 package edu.odu.cs.cs350.pne;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.io.File;
 
 public class SemesterTest {
 
@@ -13,6 +15,12 @@ public class SemesterTest {
 
     assertTrue(semester.supplyDirectories(pastDirectory, currentDirectory));
 
+ }
+ @Test
+   public void testSupplyCutoff(){
+   ClassLoader cl = getClass().getClassLoader();
+   File file1 = new File (cl.getResource("./project/src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230/dates.txt").getFile());
+   assertThat(file1).exists().isFile().canRead();
  }
   
 }
