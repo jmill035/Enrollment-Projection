f5216b346845897e7bd1ac471883cffa382bd8a8
Joshua Miller <jmill035@odu.edu>
2023-03-18 20:44:47 -0400

    changed IOReader

--- project/src/test/java/edu/odu/cs/cs350/pne/IOReaderTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/IOReaderTest.java
@@ -17,7 +17,7 @@ public class IOReaderTest {
     void testCSVReaderOutput() {
         ByteArrayOutputStrem outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        CSVReader.main(null);
+        CSVReader.ReadExcelSheets(null);
         String expectedOutput = 
         "Seats,CRN,SUBJ,CRSE,TITLE,CR HRS,XLST CAP,ENR,LINK,XLST GROUP,SCHED TYPE,,CAMPUS,INSM,PRINT?,TIME,DAYS,BLDG,ROOM,OVERRIDE,INSTRUCTOR,,OVERALL CAP,OVERALL ENR,,,PTRM START,PTRM END,WL CAP,WL,WL REMAIN,NOTES,COMMENTS,COLL \n" +
         "23","32321","CS","112","INFO LIT FORMER ENGN MAJORS","1","25","2","","SC201","A","I","B","ASYN","Y","HOURS ARR","","WEB2","","","GUPTA,REKHA","","25","2","1","ASYN","15-MAY-23","24-JUN-23","0","0","0";
