13f3a3793ca85912e48bfa84489b351183fd4ae0
eturkogl <eturk004@odu.edu>
2023-03-18 22:50:55 -0400

    added the directory paths

--- project/src/test/java/edu/odu/cs/cs350/pne/SemesterTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/SemesterTest.java
@@ -8,8 +8,8 @@ public class SemesterTest {
  public void testSupplyDirectories(){
 
     Semester semester = new Semester();
-    String pastDirectory = "path/to/past/directory" ;
-    String currentDirectory = "path/to/current/directory" ; 
+    String pastDirectory = "./project/src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230/2022*" ;
+    String currentDirectory = "./project/src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230/2023*" ; 
 
     assertTrue(semester.supplyDirectories(pastDirectory, currentDirectory));
 
