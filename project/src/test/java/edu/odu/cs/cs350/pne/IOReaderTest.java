5a573d126f6d5682eb1f549408e11a2014198038
Joshua Miller <jmill035@odu.edu>
2023-04-06 20:31:56 -0400

    fixed tests for new IOReader.java

--- project/src/test/java/edu/odu/cs/cs350/pne/IOReaderTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/IOReaderTest.java
@@ -1,32 +1,31 @@
 package edu.odu.cs.cs350.pne;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class IOReaderTest {
-
-   private final String[] filepaths = {
-
-    "project/src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230/2022-12-20.csv";
-   }
+class IOReaderTest {
     @Test
-    void testCSVReaderOutput() {
-        ByteArrayOutputStrem outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        CSVReader.ReadExcelSheets(null);
-        String expectedOutput = 
-        "Seats,CRN,SUBJ,CRSE,TITLE,CR HRS,XLST CAP,ENR,LINK,XLST GROUP,SCHED TYPE,,CAMPUS,INSM,PRINT?,TIME,DAYS,BLDG,ROOM,OVERRIDE,INSTRUCTOR,,OVERALL CAP,OVERALL ENR,,,PTRM START,PTRM END,WL CAP,WL,WL REMAIN,NOTES,COMMENTS,COLL \n" +
-        "23","32321","CS","112","INFO LIT FORMER ENGN MAJORS","1","25","2","","SC201","A","I","B","ASYN","Y","HOURS ARR","","WEB2","","","GUPTA,REKHA","","25","2","1","ASYN","15-MAY-23","24-JUN-23","0","0","0";
-        assertEquals(expectOutput, outContent.toString());
+    void testReadCSVFiles() {
+        String directoryPath = "/home/cs_jmill035/Enrollment-Projection/project/src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230";
+        File directory = new File(directoryPath);
+        assertTrue(directory.exists(), "Directory does not exist");
+        assertTrue(directory.isDirectory(), "Path is not a directory");
+
+        File[] files = directory.listFiles((dir, name) -> name.endsWith(".csv"));
+        assertNotNull(files, "No CSV files found in directory");
+        
+        for (File file : files){
+            try {
+                assertTrue(file.canRead(), "Could not read file: " + file.getName());
+            } catch (SecurityException e) {
+                fail("Security exception while accessing file: " + file.getName());
             }
+        }
+    }
 }
-
-
-
-
 
 
