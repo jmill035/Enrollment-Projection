449164223d3345c051852cab9ade2d0ef531a6af
eturkogl <eturk004@odu.edu>
2023-04-08 17:49:59 -0400

    Change files

---
+++ project/src/test/java/edu/odu/cs/cs350/pne/ProjectionReportTest.java
@@ -0,0 +1,24 @@
+package edu.odu.cs.cs350.pne;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectionReportTest {
+
+//Calculate the overall cap for all offerings of the course
+@Test
+public void calculateOverallCapTest() {
+ Offering offer1 = new Offering ("CS350", 30, 0);
+ Offering offer2 = new Offering ("CS350", 40, 0);
+ Offering offer3 = new Offering ("CS350", 50, 0);
+ 
+ List<Offering> offerings = new ArrayList<Offering>();
+
+ //add offerings 
+ offerings.add(offer1);
+ offerings.add(offer2);
+ offerings.add(offer3);
+}
+}
