19549caf7e708dc86c337e144f3c544adbd3a22f
eturkogl <eturk004@odu.edu>
2023-04-08 16:38:30 -0400

    commented an import

--- project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.util.Scanner;
+//import java.util.Scanner;
 //import java.io.FileNotFoundException; 
 import java.io.FileReader; 
 //import java.io.IOException;
