3ac586ed97523f87d1d70825260e08bcd2b1a4ea
eturkogl <eturk004@odu.edu>
2023-04-08 17:23:09 -0400

    Calculating overall test

--- project/src/test/java/edu/odu/cs/cs350/pne/OfferingTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/OfferingTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OfferingTest {
     
@@ -27,4 +29,14 @@ public class OfferingTest {
         assertThat(offer.getOverallCap(), not(is(0)));
         assertThat(offer.getOverallCap(), is(15));
     }
+}
+
+//Calculate the overall cap for all offerings of the course
+@Test
+public void calculateOverallCapTest() {
+ Offering offer1 = new Offering ("CS350", 30, 0);
+ Offering offer2 = new Offering ("CS350", 40, 0);
+ Offering offer3 = new Offering ("CS350", 50, 0);
+ 
+ List<Offering> offerings = new ArrayList<Offering>();
 }
