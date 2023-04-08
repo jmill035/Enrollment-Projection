2c66c04ac64bfbb6033b662542fea8f401ae02e3
eturkogl <eturk004@odu.edu>
2023-04-08 17:53:18 -0400

    Add public/private

--- project/src/main/java/edu/odu/cs/cs350/pne/ProjectionReport.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/ProjectionReport.java
@@ -1,2 +1,12 @@
 package edu.odu.cs.cs350.pne;
 
+import java.util.List;
+
+public class ProjectionReport {
+
+    private List<Offering> offerings;
+
+    public ProjectionReport(List<Offering> offerings) {
+        this.offerings = offerings;
+    }
+}
