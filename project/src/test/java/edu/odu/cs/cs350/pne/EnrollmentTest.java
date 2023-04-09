30246daf28cb06563aa43391a4748fea95bdbd99
r3d3k3r <lb3attd@gmail.com>
2023-04-08 23:36:49 -0400

    enrollment test fix 3

--- project/src/test/java/edu/odu/cs/cs350/pne/EnrollmentTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/EnrollmentTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.InputStream;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 
 
 public class EnrollmentTest {
