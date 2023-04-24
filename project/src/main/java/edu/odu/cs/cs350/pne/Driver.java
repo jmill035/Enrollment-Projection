0026699e46e28e9cdb9ffb0b51341762c84fd382
Logan <lblan005@odu.edu>
2023-04-24 15:28:56 -0400

    created CSV reader. List[] data

--- project/src/main/java/edu/odu/cs/cs350/pne/Driver.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Driver.java
@@ -1,20 +1,21 @@
 package edu.odu.cs.cs350.pne;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
+import com.opencsv.CSVReader;
+
 
 /*
  * 1) input args: 
  *      pos(1...n): one or more semester directory locations, indicating the locations of historical enrollment data. > input into a list
  *      pos(n-2): A single semester directory location, indicating the enrollment data for the semester for which a projection is desired.
  *      pos(n-1): A path indicating the file location where the detailed projection report will be written.
- * 2) outpu Summary Report
- * 3) ...
+ *      
+ *      --options flag YYYY-MM-DD, indicating the last day on which enrollment data from he semester being projected should be examined.
+ * 2) call csv parsing and datastructure obj
+ * 3) print summary report
  * 
  * x) Summary Report with header and output
  * 
@@ -22,12 +23,13 @@ import java.util.Scanner;
  * 
  * 
  */
-
  public class Driver {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
+
         String detailedProjectReportOutputLocation = "";
         String currentSemesterProjection = "";
-        List<String> semesterDirectories = new ArrayList<>();
+        List<String> semesterDirectories = new ArrayList<>(); // this list is from CLI input. and only directory locations
 
         int numArgs = args.length;
         if (numArgs > 2) {
@@ -42,20 +44,36 @@ import java.util.Scanner;
         System.out.println("Historical Semester Directories: " + semesterDirectories);
         System.out.println("Detailed Projection Report will be written to: " + detailedProjectReportOutputLocation);
         System.out.println("Current Semester Projection directory: " + currentSemesterProjection);
-        
 
-         // Prompt user to enter input if no arguments were provided
-         if (numArgs == 0) {
-             Scanner scanner = new Scanner(System.in);
- 
-             System.out.print("Enter the directory path for the current semester projection: ");
-             currentSemesterProjection = scanner.nextLine();
- 
-             System.out.print("Enter the file path for the detailed projection report: ");
-             detailedProjectReportOutputLocation = scanner.nextLine();
-         }
- 
-         // TODO: projection calculations
-     }
- }
- 
+        // Prompt user to enter input if no arguments were provided
+        if (numArgs == 0) {
+            System.out.print("Enter the directory path for the current semester projection: ");
+            Scanner scanner = new Scanner(System.in);
+            currentSemesterProjection = scanner.nextLine();
+
+            System.out.print("Enter the file path for the detailed projection report: ");
+            detailedProjectReportOutputLocation = scanner.nextLine();
+        }
+
+        /*
+         * Convert CSV into List object
+         */
+        String enrollmentReport = "/test/data/summary/History/202230/2022-12-20.csv";
+        try (CSVReader reader = new CSVReader(new FileReader(enrollmentReport))) {
+            List<String[]> data = new ArrayList<>();
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                String[] shortLine = Arrays.copyOf(line, 10);
+                data.add(shortLine);
+            }
+            //System.out.println(data);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // TODO: projection calculations
+        // Use: List<String[]> data as parameter
+
+    }
+}
