29266afe733cc06d638b4d2df89df01a8de2cf8c
Joshua Miller <jmill035@odu.edu>
2023-04-24 20:20:18 -0400

    made changes to IOReaderTest

--- project/src/test/java/edu/odu/cs/cs350/pne/IOReaderTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/IOReaderTest.java
@@ -1,32 +1,26 @@
 package edu.odu.cs.cs350.pne;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-import org.junit.jupiter.api.Test;
+public class IOReaderTest {
+    private static IOReader reader;
+    private static String directoryPath;
 
-class IOReaderTest {
-    @Test
-    void testReadCSVFiles() {
-       // String directoryPath = "src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230";
-        String directoryPath = "src/test/data/summary/History/202230/";
-        File directory = new File(directoryPath);
-        assertTrue(directory.exists(), "Directory does not exist");
-        assertTrue(directory.isDirectory(), "Path is not a directory");
-
-        File[] files = directory.listFiles((dir, name) -> name.endsWith(".csv"));
-        assertNotNull(files, "No CSV files found in directory");
-        
-        for (File file : files){
-            try {
-                assertTrue(file.canRead(), "Could not read file: " + file.getName());
-            } catch (SecurityException e) {
-                fail("Security exception while accessing file: " + file.getName());
-            }
-        }
+    @BeforeAll
+    static void setup(){
+        reader = new IOReader();
+        directoryPath = "./project/src/test/java/edu/odu/cs/cs350/pne/data/summary";
     }
-}
 
-
+    @Test
+    void testFileNotFound(){
+        String path = directoryPath + "/History/202230/invalid_directory";
+        Assertions.assertThrows(IOException.class, () -> reader.calculateAverages(path));
+    }
+}    
