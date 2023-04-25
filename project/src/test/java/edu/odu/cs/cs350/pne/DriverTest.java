e613cae06973b79c09d9d265d54725b67a48f7b2
Logan <lblan005@odu.edu>
2023-04-24 22:06:42 -0400

    fixed testDriver

--- project/src/test/java/edu/odu/cs/cs350/pne/DriverTest.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/DriverTest.java
@@ -12,8 +12,8 @@ public class DriverTest {
         String[] args = {"src/test/data/summary/History/202230", "/project/build/tmp/test/", "/project/build/tmp/test/"};
         Driver.main(args);
         List<String[]> data = Driver.getData();
-       // assertEquals(9, data.size()); // assuming the test data has 9 rows
-        // Print the data for verification
+       assertEquals(9, data.size()); // assuming the test data has 9 rows
+        //Print the data for verification
         System.out.println(data);
     }
 }
