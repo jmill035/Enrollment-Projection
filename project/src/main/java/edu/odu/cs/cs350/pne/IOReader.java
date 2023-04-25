58a86e609b43dca506ef694f7138f4671c202c0f
Joshua Miller <jmill035@odu.edu>
2023-04-24 20:51:35 -0400

    editted IOReader, this will be renamed later

--- project/src/main/java/edu/odu/cs/cs350/pne/IOReader.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/IOReader.java
@@ -4,15 +4,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 //consider changing into SemesterHistory/History class
 //the output of current class should be saved in a datastructure(list,etc)
 public class IOReader {
-    public void beginIOread (String[] args) {
-        String directoryPath = "./project/src/test/java/edu/odu/cs/cs350/pne/data/summary/History/202230";
+    public void beginIOread(String[] args){
+        String directoryPath = "./project/src/test/java/edu/odu/cs/cs350/pne/data/summary";
         File directory = new File(directoryPath);
-        if (!directory.exists() || !directory.isDirectory()) {
+        if (!directory.exists() || !directoryPath.isDirectory()) {
             System.out.println("Directory does not exist or is not a directory");
             return;
         }
@@ -21,47 +29,7 @@ public class IOReader {
             System.out.println("No CSV files found in directory");
             return;
         }
-        for (File file : files){
-            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-                String line;
-                while ((line = br.readLine()) != null){
-                    String[] values = line.split(","); //some commas are within quotes and faculty name abbreviations. high potential for error, recommend using openCSV
-                    for (String value : values) {
-                        System.out.print(value + ", ");
-                    }
-                    System.out.println();
-                }
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }
+        
     }
 }
 
-/*
- * original from josh's Main
- */
-// public class ReadExcelSheets {
-//     public static void main(String[] args) {
-//         String filePath = "src/data/test.xlsx";
-//         try {
-//             FileInputStream inputStream = new FileInputStream(new File(filePath))
-//             Workbook workbook = WorkbookFactory.create(inputStream);
-//             int numberOfSheets = workbook.getNumberOfSheets();
-//             for (int i = 0; i < numberOfSheets; i++;); {
-//                 Sheet sheet = workbook.getSheetAt(i);
-//                 System.out.println("Sheet Name: " + sheet.getSheetName());
-//                 for (Row row : sheet) { 
-//                     for (Cell cell : row) {
-//                         System.out.print(cell.getStringCellValue() + "\t");
-//                     }
-//                     System.out.println();
-//                 }
-//             }
-//         }
-//         workbook.close();
-//         inputStream.close();
-//     } catch (IOExceotion e){
-//         e.printStackTrace();
-//     }
-// }
